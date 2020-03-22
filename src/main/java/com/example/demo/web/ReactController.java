3b301615e6790f2bc62bb395a439c139bbf5ac3b
ozzins <ozzins@gmail.com>
2020-03-22 20:13:01 +0900

    with Thymeleaf

--- src/main/java/com/example/demo/web/ReactController.java
+++ src/main/java/com/example/demo/web/ReactController.java
@@ -3,6 +3,7 @@ package com.example.demo.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
@@ -11,18 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ReactController {
  
-    @GetMapping("/main.do")
+    @GetMapping("/test.do")
     public ModelAndView main(Model model) {
         ModelAndView mav = new ModelAndView("page");
         mav.addObject("pageName", "main");
         return mav;
     }
  
-	/*
-	@GetMapping("/{name}.do")
-	public String page(@PathVariable String name, Model model) {
-		model.addAttribute("pageName", name);
-		return "page";
-	}
-	*/
+
+    @GetMapping("/{name}.do")
+    public String page(@PathVariable String name, Model model) {
+        model.addAttribute("pageName", name);
+        return "page";
+    }
 }
