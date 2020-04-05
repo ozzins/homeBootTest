de2fb2e0cc7cc9f00071e4813d499d25e8f1a499
ozzins <ozzins@gmail.com>
2020-04-05 21:00:32 +0900

    quartz테스트항목 제거

--- src/main/java/com/example/demo/service/TestService1.java
+++ src/main/java/com/example/demo/service/TestService1.java
@@ -2,24 +2,24 @@ package com.example.demo.service;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TestService1 implements  ApplicationListener<ContextClosedEvent>//, CommandLineRunner
+public class TestService1 implements  ApplicationListener<ContextClosedEvent>, CommandLineRunner
 {
 	
-	/*
-		CommandLineRunner는 main 메서드와 거의 같다고 이해하면 된다. (run 메서드)
+	/**
+	 *	CommandLineRunner는 main 메서드와 거의 같다고 이해하면 된다. (run 메서드)
 		"스프링 애플리케이션"이 시작할 때 "1회"만 호출되는 점이 똑같고 다른 점은 static이 아니라는 점에서 이득을 가져갈 수 있는 부분이 있다. argument도 main에 들어오는 것과 똑같다!
 		애플리케이션이 시작되었을 때 하고 싶은 초기 작업이 있다면 이 인터페이스를 구현하도록 하자.
-
+	 */
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("commandLineRunner 인터페이스 구현 메서드입니다. '애플리케이션'이 실행될 때 '한 번' 실행됩니다.");
 	}
-	*/
 
 	/**
 	 * ApplicationListener는 이벤트와 관련이 있다. (onApplicationEvent 메서드)
